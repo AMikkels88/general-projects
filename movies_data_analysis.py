d601ac3593ddd2e431191fe6869814aa2793c4ba
Alex Michaels <amikkels88@gmail.com>
2023-08-04 16:32:38 +0100

    complete getting decade split movie data, next step is df analysis

--- movies_data_analysis.py
+++ movies_data_analysis.py
@@ -3,24 +3,16 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import spacy
+import logging
 
 # API details
 BASE_URL = "https://api.themoviedb.org/3"
 API_KEY = "d7bcde821afbdc346d9f46af6be295bf"
 # endpoint = "/movie/popular"
 ENDPOINT = "/discover/movie"
-# Common parameters for both time periods
-common_params = {
-    'api_key': API_KEY,
-    'vote_average.gte': 5
-}
-# Currently run 2 time periods, 1990s and 2010s
-time_periods = [
-    {'primary_release_date.gte': '1990-01-01', 'primary_release_date.lte': '2000-12-31'},
-    {'primary_release_date.gte': '2010-01-01', 'primary_release_date.lte': '2020-12-31'}
-]
-
-genre_map = {
+MAX_PAGE = 500  # set by API limits
+GENRE_MAP = {
     28: "Action",
     12: "Adventure",
     16: "Animation",
@@ -42,18 +34,32 @@ genre_map = {
     37: "Western",
 }
 
+# Common parameters for both time periods
+common_params = {
+    'api_key': API_KEY,
+    'vote_average.gte': 5
+}
+# Currently run 2 time periods, 1990s and 2010s
+time_periods = [
+    {'primary_release_date.gte': '1990-01-01', 'primary_release_date.lte': '2000-12-31'},
+    {'primary_release_date.gte': '2010-01-01', 'primary_release_date.lte': '2020-12-31'}
+]
+
+# Get data from API
 all_movies = []
 for time_period in time_periods:
     page = 1
     params = {**common_params, **time_period, 'page': page}
     
-    while True:
+    while page < MAX_PAGE:
         response = requests.get(BASE_URL + ENDPOINT, params=params)
         data = response.json()
-        movies = data['results']
-        
-        if not movies or page > 5000:
-            break
+        try:
+            movies = data['results']
+        except KeyError as e:
+            logging.error(f"Data not returned correctly, likely a page limit exceeded")
+            logging.error(f"KeyError: {e}")
+            break        
         
         all_movies.extend(movies)
         print(movies[0]["release_date"])
@@ -61,7 +67,7 @@ for time_period in time_periods:
         page += 1
         params['page'] = page
 
-# Create DataFrame
+# Data processing
 df = pd.DataFrame(all_movies)
 df.drop(columns=[
     "adult",
@@ -77,11 +83,10 @@ genre_counts = df["genre_ids"].explode().value_counts()
 # Flatten genre_ids list
 df["genre_ids"] = df["genre_ids"].apply(lambda x: [int(genre_id) for genre_id in x])
 df["genre_map"] = df["genre_ids"].apply(
-    lambda x: [genre_map[genre_id] for genre_id in x]
+    lambda x: [GENRE_MAP[genre_id] for genre_id in x]
 )
 
 # TODO:
-# get more pages from the api (years specified would be good)
 # perform some basic genre analysis
 # use NLP (spaCy / scikit)? to analyse the title text and maybe overview text, looking
 # for repeated franchise words, numbered movies etc, to get a sense of rate of sequels / remakes
